584aa4e5353717ec3a36414058aa1ee152a779fb
DenysVodzyk <vodzyk@gmail.com>
2021-03-26 21:55:04 -0400

    Customer entity

---
+++ src/main/java/com/example/hibernatetest/entity/Customer.java
@@ -0,0 +1,106 @@
+package com.example.hibernatetest.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "ccNo")
+    private String ccNo;
+    @Column(name = "ccType")
+    private String ccType;
+    @Column(name = "maturity")
+    private java.sql.Date maturity;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String email, String ccNo, String ccType, Date maturity) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.ccNo = ccNo;
+        this.ccType = ccType;
+        this.maturity = maturity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCcNo() {
+        return ccNo;
+    }
+
+    public void setCcNo(String ccNo) {
+        this.ccNo = ccNo;
+    }
+
+    public String getCcType() {
+        return ccType;
+    }
+
+    public void setCcType(String ccType) {
+        this.ccType = ccType;
+    }
+
+    public Date getMaturity() {
+        return maturity;
+    }
+
+    public void setMaturity(Date maturity) {
+        this.maturity = maturity;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", ccNo='" + ccNo + '\'' +
+                ", ccType='" + ccType + '\'' +
+                ", maturity=" + maturity +
+                '}';
+    }
+}
