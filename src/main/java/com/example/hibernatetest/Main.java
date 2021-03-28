e1a6020dc650fbedb1130f25d94ac630e597a6fc
DenysVodzyk <vodzyk@gmail.com>
2021-03-28 14:00:27 -0400

    minor changes

--- src/main/java/com/example/hibernatetest/Main.java
+++ src/main/java/com/example/hibernatetest/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        IMerchantService merchantService = context.getBean(MerchantService.class);
+        IMerchantService merchantService = context.getBean(IMerchantService.class);
         ICustomerService customerService = context.getBean(ICustomerService.class);
 
         System.out.println(merchantService.findById(1));
@@ -21,8 +21,18 @@ public class Main {
 
         Customer elon = new Customer("Elon Musk", "3500 Deer Creek, Palo Alto, CA 94304",
                 "NAsales@teslamotors.com", "1111111111111111", "Visa", java.sql.Date.valueOf("2022-10-22"));
+        Merchant merchant = new Merchant("Steven", "Scotia", "AA245BXW", "247991002", 5.1,
+                1, 100.0, 50.0, 1.0, java.sql.Date.valueOf("2022-10-22"));
 
-        customerService.save(elon);
+
+//        customerService.save(elon);
         System.out.println("Id: " + elon.getId());
+//
+//        merchantService.save(merchant);
+//        System.out.println("Id: " + merchant.getId());
+//
+//        System.out.println(customerService.remove(15));
+
+        System.out.println(customerService.remove(customerService.findById(21)));
     }
 }
