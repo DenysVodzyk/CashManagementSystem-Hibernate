606c1fba8193bb2b9857d1a9a96cee56a4b6efee
DenysVodzyk <vodzyk@gmail.com>
2021-03-28 13:58:00 -0400

    added save method

--- src/main/java/com/example/hibernatetest/repository/IMerchantRepo.java
+++ src/main/java/com/example/hibernatetest/repository/IMerchantRepo.java
@@ -1,7 +1,10 @@
 package com.example.hibernatetest.repository;
 
+import com.example.hibernatetest.entity.Customer;
 import com.example.hibernatetest.entity.Merchant;
 
 public interface IMerchantRepo {
     public Merchant findById(int id);
+
+    public void save(Merchant merchant);
 }
