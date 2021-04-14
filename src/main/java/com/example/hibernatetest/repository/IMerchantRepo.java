6cb85b75378f3c1cc8c6d62c250faee6993acb5c
DenysVodzyk <vodzyk@gmail.com>
2021-04-13 20:07:12 -0400

    added get methods

--- src/main/java/com/example/hibernatetest/repository/IMerchantRepo.java
+++ src/main/java/com/example/hibernatetest/repository/IMerchantRepo.java
@@ -2,9 +2,16 @@ package com.example.hibernatetest.repository;
 
 import com.example.hibernatetest.entity.Customer;
 import com.example.hibernatetest.entity.Merchant;
+import com.example.hibernatetest.entity.dto.Result;
+
+import java.util.List;
 
 public interface IMerchantRepo {
     public Merchant findById(int id);
 
     public void save(Merchant merchant);
+
+    public List<Result> getTotalReport();
+
+    public List<Merchant> getSortedByNeedToPay();
 }
