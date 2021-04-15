0d797bfb86f7e79a3615214758b72a1350a83f96
DenysVodzyk <vodzyk@gmail.com>
2021-04-14 22:54:07 -0400

    get methods implementation

--- src/main/java/com/example/hibernatetest/service/MerchantService.java
+++ src/main/java/com/example/hibernatetest/service/MerchantService.java
@@ -1,11 +1,13 @@
 package com.example.hibernatetest.service;
 
 import com.example.hibernatetest.entity.Merchant;
+import com.example.hibernatetest.entity.dto.Result;
 import com.example.hibernatetest.repository.MerchantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class MerchantService implements IMerchantService {
@@ -22,4 +24,16 @@ public class MerchantService implements IMerchantService {
     public void save(Merchant merchant) {
         merchantRepo.save(merchant);
     }
+
+    @Override
+    public List<Result> getTotalReport() {
+        return merchantRepo.getTotalReport();
+    }
+
+    @Override
+    public List<Merchant> getSortedByNeedToPay() {
+        return merchantRepo.getSortedByNeedToPay();
+    }
+
+
 }
