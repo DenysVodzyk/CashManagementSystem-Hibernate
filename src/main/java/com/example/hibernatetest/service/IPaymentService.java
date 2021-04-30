773b2bbd2d724befd63bb88607ef71d4ec7b3612
DenysVodzyk <vodzyk@gmail.com>
2021-04-29 23:42:37 -0400

    added selectByCustomerId method

--- src/main/java/com/example/hibernatetest/service/IPaymentService.java
+++ src/main/java/com/example/hibernatetest/service/IPaymentService.java
@@ -1,8 +1,13 @@
 package com.example.hibernatetest.service;
 
+import com.example.hibernatetest.entity.Customer;
 import com.example.hibernatetest.entity.Payment;
+
+import java.util.List;
 
 public interface IPaymentService {
 
     public Payment findById(int id);
+
+    public List<Customer> selectByCustomerId(int id);
 }
