49c8615a68c2601d7f19dcb037ca3ffbf8217f2f
DenysVodzyk <vodzyk@gmail.com>
2021-04-30 22:40:40 -0400

    added more report methods

--- src/main/java/com/example/hibernatetest/service/IPaymentService.java
+++ src/main/java/com/example/hibernatetest/service/IPaymentService.java
@@ -9,5 +9,9 @@ public interface IPaymentService {
 
     public Payment findById(int id);
 
-    public List<Customer> selectByCustomerId(int id);
+    public List<Payment> selectByCustomerId(int id);
+
+    public double getPaymentsSum();
+
+    public List<Payment> getPaymentsAboveLimit(double limit);
 }
